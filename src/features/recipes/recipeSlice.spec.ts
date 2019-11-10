3f069bdb29145fe3bd0b00f21b2238b87ded5a6e
Ben Campbell <benjamin.campbell@mines.sdsmt.edu>
2019-11-10 03:15:33 -0700

    Ran prettier to fix style issues

--- src/features/recipes/recipeSlice.spec.ts
+++ src/features/recipes/recipeSlice.spec.ts
@@ -9,9 +9,18 @@ import recipeReducer, {
   availabilityStateMap,
 } from './recipeSlice'
 
-const dummyRecipe: Recipe = { title: 'Dummy Recipe', description: 'Dummy Recipe Description' },
-  dummyRecipe2: Recipe = { title: 'Dummy Recipe 2', description: 'Dummy Recipe 2 Description' },
-  dummyRecipe3: Recipe = { title: 'Dummy Recipe 3', description: 'Dummy Recipe 3 Description' }
+const dummyRecipe: Recipe = {
+    title: 'Dummy Recipe',
+    description: 'Dummy Recipe Description',
+  },
+  dummyRecipe2: Recipe = {
+    title: 'Dummy Recipe 2',
+    description: 'Dummy Recipe 2 Description',
+  },
+  dummyRecipe3: Recipe = {
+    title: 'Dummy Recipe 3',
+    description: 'Dummy Recipe 3 Description',
+  }
 
 describe('recipe reducer', () => {
   it('should handle prevRecipe', () => {

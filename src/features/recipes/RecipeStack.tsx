464f0767ec692d592ece95f67d2e43852ec11ed0
Brian Brunner <brian.brunner.bb@gmail.com>
2019-11-13 00:15:15 -0700

    Add quotes around glob
    Misinterpreted on some machines, adding quotes seems to fix it

--- src/features/recipes/RecipeStack.tsx
+++ src/features/recipes/RecipeStack.tsx
@@ -27,7 +27,4 @@ const RecipeList: React.FC<Props> = ({ recipe }) => (
   </div>
 )
 
-export default connect(
-  mapState,
-  mapDispatch
-)(RecipeList)
+export default connect(mapState, mapDispatch)(RecipeList)

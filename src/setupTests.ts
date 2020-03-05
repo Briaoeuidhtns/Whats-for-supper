1f6c9955a0661c3600a50a2c4eadfd97c13c6b0f
Brian Brunner <brian.brunner.bb@gmail.com>
2020-03-04 22:48:12 -0700

    Make ci fail on type error in tests
    For some reason compile errors in tests are silently ignored. Run type checker
    independently

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,3 +1,6 @@
 global.console.error = global.console.warn = (msg: any, ...rest: any[]) => {
   throw rest.length ? [msg, ...rest] : msg
 }
+
+// Explicitly silence `error TS1208: All files must be modules when the '--isolatedModules' flag is provided.`
+export {}

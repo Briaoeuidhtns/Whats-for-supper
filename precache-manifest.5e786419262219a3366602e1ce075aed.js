9bcb9a589cdf60b975a6e38a00d65b0a8990fcff
Brian Brunner <brian.brunner.bb@gmail.com>
2020-04-11 17:01:58 -0600

    upload build

---
+++ precache-manifest.5e786419262219a3366602e1ce075aed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a2d4051e3d705e655f23a35437b2295",
+    "url": "/Whats-for-supper/index.html"
+  },
+  {
+    "revision": "19c0bdd2669c0ce9e125",
+    "url": "/Whats-for-supper/static/js/2.fd25f3fd.chunk.js"
+  },
+  {
+    "revision": "04e60af1d6ecfee5f1aabc2f67b9a2c3",
+    "url": "/Whats-for-supper/static/js/2.fd25f3fd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "365f8b51b05009d5956f",
+    "url": "/Whats-for-supper/static/js/main.4d1012e1.chunk.js"
+  },
+  {
+    "revision": "dfa9a2506adde43359be",
+    "url": "/Whats-for-supper/static/js/runtime-main.a1e9c9d3.js"
+  }
+]);
